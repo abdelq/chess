ed61c0d85be651c1a96d9425cee32707c019a204
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-02-24 13:17:55 -0500

    Added some todos

--- src/chess/JeuEchec.java
+++ src/chess/JeuEchec.java
@@ -7,6 +7,10 @@ package chess;
 public class JeuEchec {
 
     public static void main(String[] args) {
+        // TODO Paramètre sur la ligne de commande qui sera soit "ascii" soit "unicode" (et qui déterminera le type d'affichage)
+        Echiquier echiquier = new Echiquier();
+        // TODO Demander tour à tour à chaque joueur (en commençant par les blancs) d'indiquer le déplacement qu'ils veulent jouer
+        // TODO Reste du document
     }
 
 }
