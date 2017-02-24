d12eb753e74ffcc35ebe33cd67143861953177df
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-02-24 11:14:05 -0500

    Basic version of Piece

---
+++ src/chess/Echiquier.java
@@ -0,0 +1,24 @@
+package chess;
+
+/**
+ * @author Léo Jetzer
+ * @author Abdelhakim Qbaich
+ */
+class Echiquier {
+
+    boolean caseValide(int nouvelle_colonne, int nouvelle_ligne) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    boolean examinePiece(int nouvelle_colonne, int nouvelle_ligne) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    void prendsPiece(int nouvelle_colonne, int nouvelle_ligne) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    void posePiece(Piece aThis) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
---
+++ src/chess/Piece.java
@@ -0,0 +1,71 @@
+package chess;
+
+/**
+ * @author Léo Jetzer
+ * @author Abdelhakim Qbaich
+ */
+public abstract class Piece {
+
+    private boolean est_blanc, est_capture;
+    private int colonne, ligne;
+    private Echiquier echiquier;
+
+    public boolean estBlanc() {
+        return est_blanc;
+    }
+
+    public boolean estNoir() {
+        return !est_blanc;
+    }
+
+    public boolean estCapture() {
+        return est_capture;
+    }
+
+    public int getLigne() {
+        return ligne;
+    }
+
+    public int getColonne() {
+        return colonne;
+    }
+
+    protected Piece(boolean est_blanc, int colonne, int ligne, Echiquier echiquier) {
+        this.est_blanc = est_blanc;
+        this.colonne = colonne;
+        this.ligne = ligne;
+        this.echiquier = echiquier;
+        this.est_capture = false;
+    }
+
+    void meSuisFaitCapture() {
+        est_capture = true;
+    }
+
+    boolean deplacementValide(int nouvelle_colonne, int nouvelle_ligne) {
+        return !est_capture
+                && echiquier.caseValide(nouvelle_colonne, nouvelle_ligne);
+        // && echiquier.examinePiece(nouvelle_colonne, nouvelle_ligne);
+    }
+
+    void deplace(int nouvelle_colonne, int nouvelle_ligne) {
+        echiquier.prendsPiece(nouvelle_colonne, nouvelle_ligne);
+
+        colonne = nouvelle_colonne;
+        ligne = nouvelle_ligne;
+        
+        // echiquier.capturePiece();
+        
+        echiquier.posePiece(this);
+    }
+    
+    @Override
+    public String toString() {
+        // retournera une représentation sous fore de chaine de l'état de la piece (tous ses attributs à l'exception de l'echiquier)
+    }
+    
+    abstract public String representationAscii();
+    
+    abstract public String representationUnicode();
+
+}
